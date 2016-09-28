d5dfb8efdb401a1649e56c9596fe17e64303f302
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-09-28 17:47:03 +0400

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def split4(arr):
+    a1, a2 = np.hsplit(arr, 2)
+    return np.vsplit(a1, 2) + np.vsplit(a2, 2)
+
+
+def mult(a, b):
+    n = np.shape(a)[0]
+    if n == 1:
+        return a * b
+    a11, a21, a12, a22 = split4(a)
+    b11, b21, b12, b22 = split4(b)
+    p1 = mult(a11 + a22, b11 + b22)
+    p2 = mult(a21 + a22, b11)
+    p3 = mult(a11, b12 - b22)
+    p4 = mult(a22, b21 - b11)
+    p5 = mult(a11 + a12, b22)
+    p6 = mult(a21 - a11, b11 + b12)
+    p7 = mult(a12 - a22, b21 + b22)
+
+    c11 = p1 + p4 - p5 + p7
+    c12 = p3 + p5
+    c21 = p2 + p4
+    c22 = p1 - p2 + p3 + p6
+    return np.vstack((np.hstack((c11, c12)),
+                      np.hstack((c21, c22))))
+n = int(input())
+near2 = 1
+while near2 < n:
+    near2 = near2 * 2
+aplus0 = bplus0 = np.zeros((near2, near2), int)
+a = []
+b = []
+for i in range(n):
+    a.append(list(map(int, input().split())))
+for i in range(n):
+    b.append(list(map(int, input().split())))
+aplus0[:n, :n] = np.array(a)
+bplus0[:n, :n] = np.array(b)
+c = mult(aplus0, bplus0)
+
+for i in c[:n,:n]:
+    print(*i)
