75960221557c8d6f6aea43f88da154161a247074
iisuslik43 <al.s.fedotov1998@yandex.ru>
2016-10-05 22:56:11 +0400

    Add files via upload

--- HW2.py
+++ HW2.py
@@ -11,9 +11,9 @@ def md5(path):
     return digest.hexdigest()
 
 hash_to_files = collections.defaultdict(list)
-for path, _, files in os.walk(os.path.abspath(sys.argv[1])):
+for directory, _, files in os.walk(os.path.abspath(sys.argv[1])):
     for file in files:
-        path_to_file = os.path.join(path, file)
+        path_to_file = os.path.join(directory, file)
         if not file.startswith(('.', '~')) and not os.path.islink(path_to_file):
             hash_to_files[md5(path_to_file)].append(path_to_file)
 
